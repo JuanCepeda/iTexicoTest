83876b3e0ad4993983694b767a785a7ded83e546
Juan Cepeda <juan.emmanuel.cepeda@gmail.com>
2019-04-04 20:07:00 -0400

    Initial Commit

---
+++ CodeTestEmmanuelCepeda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeTestEmmanuelCepeda
+//
+//  Created by Juan Cepeda on 4/4/19.
+//  Copyright © 2019 Juan Cepeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
